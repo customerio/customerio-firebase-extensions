76939f0af7c63d2b262a8fb681d0b1984e2aec53
Colby Ausen <colby@customer.io>
2023-03-19 16:26:24 -0600

    Lint

--- auth-customerio/functions/src/index.js
+++ auth-customerio/functions/src/index.js
@@ -1,37 +1,37 @@
 import * as functions from "firebase-functions";
 
-import { TrackClient, RegionUS, RegionEU } from "customerio-node";
+import {TrackClient, RegionUS, RegionEU} from "customerio-node";
 
 const SITE_ID = process.env.CIO_SITE_ID;
 const API_KEY = process.env.CIO_API_KEY;
 const REGION = process.env.CIO_REGION == "us" ? RegionUS : RegionEU;
 const IDENTIFIER = process.env.CIO_IDENTIFIER;
 
-let cio = new TrackClient(SITE_ID, API_KEY, { region: REGION });
+const cio = new TrackClient(SITE_ID, API_KEY, {region: REGION});
 
 export const createProfile = functions.auth.user().onCreate(async (user) => {
   functions.logger.log("Firebase user created", user);
 
   let id = "";
-  if(IDENTIFIER == "id" && user.uid != null && user.uid != "") {
+  if (IDENTIFIER == "id" && user.uid != null && user.uid != "") {
     id = user.id;
   }
-  if(IDENTIFIER == "email" && user.email != null && user.email != "") {
+  if (IDENTIFIER == "email" && user.email != null && user.email != "") {
     id = user.email;
   }
 
-  if(id=="") {
+  if (id=="") {
     functions.logger.error("can't create CIO profile without uid or email");
     return;
   }
 
   cio.identify(id, {}).then(
-    () => {
-      functions.logger.log("created", { id });
-    },
-    (err) => {
-      functions.logger.error("create failed", err);
-    }
+      () => {
+        functions.logger.log("created", {id});
+      },
+      (err) => {
+        functions.logger.error("create failed", err);
+      },
   );
 });
 
@@ -39,24 +39,24 @@ export const deleteProfile = functions.auth.user().onDelete(async (user) => {
   functions.logger.log("Firebase user deleted", user);
 
   let id = "";
-  if(IDENTIFIER == "id" && user.uid != null && user.uid != "") {
+  if (IDENTIFIER == "id" && user.uid != null && user.uid != "") {
     id = user.id;
   }
-  if(IDENTIFIER == "email" && user.email != null && user.email != "") {
+  if (IDENTIFIER == "email" && user.email != null && user.email != "") {
     id = user.email;
   }
 
-  if(id=="") {
+  if (id=="") {
     functions.logger.error("can't delete CIO profile without uid or email");
     return;
   }
 
   cio.destroy(id, {}).then(
-    () => {
-      functions.logger.log("deleted", { id });
-    },
-    (err) => {
-      functions.logger.error("delete failed", err);
-    }
+      () => {
+        functions.logger.log("deleted", {id});
+      },
+      (err) => {
+        functions.logger.error("delete failed", err);
+      },
   );
 });
