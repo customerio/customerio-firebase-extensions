ab95dd07bda60dfad24a8fdcfb93c87450cfba4f
Colby Ausen <colby@customer.io>
2023-04-13 16:49:06 -0600

    Stub out test framework

---
+++ sync-customerio/functions/src/auth_handler.spec.ts
@@ -0,0 +1,5 @@
+describe("Auth Handler Tests", () => {
+    it("should run", () => {
+        // we need a test here
+   });
+});
---
+++ sync-customerio/functions/src/utils.spec.ts
@@ -0,0 +1,9 @@
+describe("Utils Tests", () => {
+    it("should use id if given", () => {
+        // write a test
+    });
+
+    it("should use email if given", () => {
+        // write a test, geez!
+    });
+});
